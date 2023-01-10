10fabbaa1e4a8d05e532ab62f98239a312c2dec7
BananaRaptor <arnaudthebergemandeville@gmail.com>
2023-01-10 17:38:10 -0500

    initial commit

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
-# AR.js Testing
+# Basic location based tracking
 
 Testing AR.js functionality
 
-Image used for tracking: [Link](https://raw.githubusercontent.com/AR-js-org/AR.js/master/data/images/hiro.png)
+You'll need to use your cellphone and a certificate to allow your phone sensor to work see : 
+https://web.dev/how-to-use-local-https/ for mkcert tutorial
+https://apple.stackexchange.com/questions/123988/how-to-add-a-crt-certificate-to-iphones-keychain for iphone certificate
--- server.js
+++ server.js
@@ -1,14 +1,38 @@
 const express = require('express')
+var fs = require('fs');
+var https = require('https');
 const app = express()
 const morgan = require('morgan')
 const path = require("path")
+var https = require('https');
+
+// Not included in repo generate some using certbot
+var privateKey = fs.readFileSync('localhost+2-key.pem');
+var certificate = fs.readFileSync('localhost+2.pem');
+
+var options = {
+    cert: certificate,
+    key: privateKey
+};
+
+
 app.use(morgan('dev'))
 app.use(express.static(path.join(__dirname, "/")))
 app.use(express.json())
-app.use(express.urlencoded({extended: false}))
-app.get('/', (req,res) => {
+app.use(express.urlencoded({
+    extended: false
+}))
+app.get('/', (req, res) => {
     res.render('index')
 })
+
+var server = https.createServer(options, app);
+
 app.listen(3000, () => {
     console.log('listening on port 3000')
 })
+
+// HTTPS endpoint to allow cellphone sensor on IOS
+server.listen(8001, function () {
+    console.log("server running at https://IP_ADDRESS:8001/")
+});
